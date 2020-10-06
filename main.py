f5518650756afbb0f2d0854e7d446a13d8b3c673
Toast <72392833+ReallyToast@users.noreply.github.com>
2020-10-06 17:55:09 -0400

    Fixed main script
    - Now should work with any os

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import sys, os
+from os import system, name
 import time
-clear = lambda: os.system('clear')
 
-r, c = os.popen('stty size', 'r').read().split()
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+import shutil
+c, r = shutil.get_terminal_size(fallback=(80, 24))
 p = "\033[1;94m{}:-^{}{}\033[0;0m".format("{", int(c), "}").format("")
 
 while True:
   clear()
   print(p)
-  print("\033[0;93m", "Welcome to the Programs Hub!".center(int(c)), "\033[0m")
-  print("\033[1;91m", "Type the filename to start", "\033[0;0m")
+  print("\033[0;93m", "Welcome to the Program Center!".center(int(c)), "\033[0m")
+  print("\033[1;91m", "Type the filename to start", "\033[0;0m", "\033[1;91m", "Type 'End' or 'Stop' to Exit", "\033[0;0m")
   print(p)
 
   filename = input()
-  if os.path.exists("Programs/{}".format(filename)):
+  if filename.lower() == "end" or filename.lower() == "stop":
+    clear()
+    exit()
+  elif os.path.exists("Programs/{}".format(filename)):
     clear()
     exec(open("Programs/{}".format(filename)).read())
   else:
