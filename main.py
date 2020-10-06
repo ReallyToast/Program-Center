faa3bd60437c5180a6ca0ed3943974c6bf0db568
Toast <xavier.moody@msad59.org>
2020-10-06 20:57:38 +0000

    Added Main Executor

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-import pygame
+import sys, os
+import time
+clear = lambda: os.system('clear')
 
+r, c = os.popen('stty size', 'r').read().split()
+p = "\033[1;94m{}:-^{}{}\033[0;0m".format("{", int(c), "}").format("")
+
+while True:
+  clear()
+  print(p)
+  print("\033[0;93m", "Welcome to the Programs Hub!".center(int(c)), "\033[0m")
+  print("\033[1;91m", "Type the filename to start", "\033[0;0m")
+  print(p)
+
+  filename = input()
+  if os.path.exists("Programs/{}".format(filename)):
+    clear()
+    exec("Programs/{}".format(filename))
+  else:
+    print("\033[1;91m", "File not found!", "\033[0;0m")
+    time.sleep(2)
